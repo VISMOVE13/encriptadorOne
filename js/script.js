ea0f8150ef644df2dd35fa1a1a3b56d3b8fe067a
VISMOVE13 <move1309@gmail.com>
2024-08-26 22:31:38 -0500

    Agregada función desencriptar y opcion de copiar texto del sidebar

--- js/script.js
+++ js/script.js
@@ -6,11 +6,15 @@ const botonDesencriptar = document.getElementById("botonDesencriptar");
 
 const botonCopiar = document.getElementById("botonCopiar");
 
+const mensajefinal = document.getElementById("mensajeFinal");
+
 //e - enter
 //i - imes
 //a - ai
 //o - ober
 //u - ufat
+
+
 
 let remplazar = [
     ["e", "enter"],
@@ -20,22 +24,54 @@ let remplazar = [
     ["u", "ufat"],
 ];
 
+
 botonEncriptar.addEventListener('click', () => {
+    
     const texto = ingresoTexto.value.toLowerCase();
+    console.log({texto})
+
+    // Definición de la función encriptar
     function encriptar(newText) { 
         for (let i = 0; i < remplazar.length; i++){
             if (newText.includes(remplazar[i][0])){
-                newTex = newText.replaceAll(remplazar[i][0], 
+                newText = newText.replaceAll(remplazar[i][0], 
                     remplazar[i][1]);
             };
         };
         return newText;
     }
-   
-
-    console.log(encriptar(texto));
+   const textoEncriptado = encriptar(texto);
+   console.log({ textoEncriptado })
+   mensajefinal.innerHTML = textoEncriptado;
+   botonCopiar.classList.replace("oculto", "visible")
+    //console.log(encriptar(texto));
 });
 
+botonDesencriptar.addEventListener('click', () => {
+
+	const textoParaDesencriptar = ingresoTexto.value.toLowerCase();
+
+	if (textoParaDesencriptar != "") {
+		function desencriptar(newtext) {
+			for (let i = 0; i < remplazar.length; i++) {
+				if (newtext.includes(remplazar[i][1])) {
+					newtext = newtext.replaceAll(remplazar[i][1], remplazar[i][0]);
+				};
+			};
+			return newtext;
+		};
+		const textoDesencriptado = desencriptar(textoParaDesencriptar);
+        mensajefinal.innerHTML = textoDesencriptado;
+	} else {
+		alert("Ingrese el texto a desencriptar");
+	};
+});
+
+botonCopiar.addEventListener('click', () => {
+    const textoACopiar = mensajefinal.innerText;
+    navigator.clipboard.writeText(textoACopiar);
+    alert(`Texto copiado al porta papeles: ${textoACopiar}`);
+});
 
 
 
